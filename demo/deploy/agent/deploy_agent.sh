d5e25e50c5e5111bfb2595b771dc2865a351d6bc
Nick Cain <nicholascain@google.com>
2022-09-16 19:49:46 +0000

    update deploy_agent

--- demo/deploy/agent/deploy_agent.sh
+++ demo/deploy/agent/deploy_agent.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-SHORT=r::,p::,h
-LONG=region::,project_id::,help
+SHORT=r::,p::,w::,h
+LONG=region::,project_id::,webhook_name::,help
 OPTS=$(getopt -a --options $SHORT --longoptions $LONG -- "$@")
 
 help()
 {
     echo "Usage: deploy_agent [ -r | --region     ]
-                    [ -p | --project_id ]
-                    [ -h | --help       ]"
-
+                    [ -p | --project_id   ]
+                    [ -w | --webhook_name ]
+                    [ -h | --help         ]"
     exit 2
 }
 
@@ -33,6 +33,10 @@ do
       PROJECT_ID="$2"
       shift 2
       ;;
+    -w | --webhook_name )
+      WEBHOOK_NAME="$2"
+      shift 2
+      ;;
     -h | --help)
       help
       ;;
@@ -47,8 +51,7 @@ do
   esac
 done
 
-echo $REGION
-echo $PROJECT_ID
+WEBHOOK_TRIGGER_URI="https://${REGION?}-${PROJECT_ID?}.cloudfunctions.net/${WEBHOOK_NAME?}"
 
 curl -s -X POST \
   -H "Authorization: Bearer $(gcloud auth print-access-token)" \
@@ -76,3 +79,14 @@ curl -s -X POST \
     "agentUri": "gs://gassets-api-ai/prebuilt_agents/cx-prebuilt-agents/exported_agent_Telecommunications.blob"
   }' \
   "https://${REGION?}-dialogflow.googleapis.com/v3/${AGENT_NAME?}:restore"
+
+curl -s -X PATCH\
+  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
+  -H "Content-Type:application/json" \
+  -H "x-goog-user-project: ${PROJECT_ID?}" \
+  -d \
+  "{
+    \"displayName\": \"cxPrebuiltAgentsTelecom\",
+    \"genericWebService\": {\"uri\": \"${WEBHOOK_TRIGGER_URI?}\"}
+  }" \
+  "https://${REGION?}-dialogflow.googleapis.com/v3/${WEBHOOK_NAME?}"
