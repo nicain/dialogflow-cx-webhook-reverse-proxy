6cce380bc9dbbdde20123bde018209597a873e7b
Nick Cain <nicholascain@google.com>
2022-09-16 18:48:03 +0000

    terraform webhook

---
+++ demo/terraform/webhook/main.tf
@@ -0,0 +1,98 @@
+
+
+variable "access_token" {
+  description = "Access Token"
+  type        = string
+}
+
+variable "project_id" {
+  description = "Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Region"
+  type        = string
+}
+
+variable "bucket" {
+  description = "bucket"
+  type        = string
+}
+
+variable "webhook_name" {
+  description = "webhook_name"
+  type        = string
+}
+
+output "project_id" {
+  value = var.project_id
+}
+
+output "access_token" {
+  value = var.access_token
+}
+
+output "region" {
+  value = var.region
+}
+
+output "bucket" {
+  value = var.bucket
+}
+
+output "webhook_name" {
+  value = var.webhook_name
+}
+
+locals {
+	root_dir = abspath("./")
+  archive_path = abspath("./tmp/function.zip")
+  region = var.region
+}
+
+data "archive_file" "source" {
+  type        = "zip"
+  source_dir  = abspath("./webhook")
+  output_path = local.archive_path
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = var.bucket
+  location = "US"
+  project = var.project_id
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  access_token = var.access_token
+}
+
+terraform {
+  required_providers {
+    google = "~> 3.17.0"
+  }
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "index.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = data.archive_file.source.output_path
+  depends_on = [data.archive_file.source, google_storage_bucket.bucket]
+}
+
+resource "google_cloudfunctions_function" "function" {
+  project = var.project_id
+  name        = var.webhook_name
+  description = "VPC-SC Demo Webhook"
+  runtime     = "python39"
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  timeout               = 60
+  entry_point           = "cxPrebuiltAgentsTelecom"
+  region = var.region
+  depends_on = [google_storage_bucket_object.archive]
+}
